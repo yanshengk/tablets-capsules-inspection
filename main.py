daf2cef973bd7d98d4a0f0f585bf001cae31fea1
Kang Yan Sheng <yanshengk2002@hotmail.com>
2022-09-07 13:16:24 +0800

    move functions from main.py to my_function.py

--- main.py
+++ main.py
@@ -2,8 +2,7 @@ import RPi.GPIO as GPIO
 import os
 from datetime import datetime
 import cv2
-import imutils
-import numpy as np
+# import numpy as np
 # from matplotlib import pyplot as plt
 import my_function as mf
 import my_process as mp
@@ -66,49 +65,6 @@ def img_capture(ev, preview=False, duration=1500):
     return current_timestamp
 
 
-def img_resize(image, factor):
-    width = int(image.shape[1] * factor)
-    image_resized = imutils.resize(image, width=width)
-
-    return image_resized
-
-
-def stack_images(scale, img_array):
-    rows = len(img_array)
-    cols = len(img_array[0])
-    rows_available = isinstance(img_array[0], list)
-    width = img_array[0][0].shape[1]
-    height = img_array[0][0].shape[0]
-    if rows_available:
-        for x in range(0, rows):
-            for y in range(0, cols):
-                if img_array[x][y].shape[:2] == img_array[0][0].shape[:2]:
-                    img_array[x][y] = cv2.resize(img_array[x][y], (0, 0), None, scale, scale)
-                else:
-                    img_array[x][y] = cv2.resize(img_array[x][y], (img_array[0][0].shape[1], img_array[0][0].shape[0]),
-                                                 None, scale, scale)
-                if len(img_array[x][y].shape) == 2:
-                    img_array[x][y] = cv2.cvtColor(img_array[x][y], cv2.COLOR_GRAY2BGR)
-        image_blank = np.zeros((height, width, 3), np.uint8)
-        hor = [image_blank] * rows
-        # hor_con = [image_blank] * rows
-        for x in range(0, rows):
-            hor[x] = np.hstack(img_array[x])
-        ver = np.vstack(hor)
-    else:
-        for x in range(0, rows):
-            if img_array[x].shape[:2] == img_array[0].shape[:2]:
-                img_array[x] = cv2.resize(img_array[x], (0, 0), None, scale, scale)
-            else:
-                img_array[x] = cv2.resize(img_array[x], (img_array[0].shape[1], img_array[0].shape[0]), None, scale,
-                                          scale)
-            if len(img_array[x].shape) == 2:
-                img_array[x] = cv2.cvtColor(img_array[x], cv2.COLOR_GRAY2BGR)
-        hor = np.hstack(img_array)
-        ver = hor
-    return ver
-
-
 def main():
     # Start conveyor until carriage is detected
     while True:
@@ -133,7 +89,7 @@ def main():
     result, count, condition = mp.process_image(img, path, 3)  # 1-WhiteTab, 2-PinkTab, 3-YellowGreenCap
 
     # Show output
-    cv2.imshow("Contours", img_resize(result, 0.40))
+    cv2.imshow("Contours", mf.img_resize(result, 0.40))
     mf.print_message(f"{count} object(s) detected", "INFO")
     if condition:
         mf.print_message("PASSED", "INFO")
